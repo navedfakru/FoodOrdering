0f70fa41efecebb3e2fef3b7c3bb7a0938e5fc4d
Naved Ali <naved10055@gmail.com>
2024-07-21 22:32:50 +0530

    Flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,43 +1,49 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import Colors from '@/src/constants/Colors';
-import { Product } from '../types';
+import { Image, StyleSheet, Text, View } from "react-native";
+import Colors from "@/src/constants/Colors";
+import { Product } from "../types";
 
-export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
   product: Product;
-}
+};
 
-const ProductListItem = ({product}: ProductListItemProps) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
-
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode='contain'
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
-  )
-}
+  );
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
-    borderRadius: 20
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
-  title: { 
-    fontSize: 18, 
-    fontWeight: '600' ,
-    marginVertical: 10
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   image: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 1,
-  }
+  },
 });
